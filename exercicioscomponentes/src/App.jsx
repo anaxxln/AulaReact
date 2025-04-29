29408c5567d39c48054557d50a58d6e710f12934
anaxxln <anaj96623@gmail.com>
2025-04-29 12:24:45 -0300

    29/04

--- exercicioscomponentes/src/App.jsx
+++ exercicioscomponentes/src/App.jsx
@@ -2,6 +2,8 @@ import Exemplo1 from "./Componentes/Exemplo1"
 import Exercicio1 from "./componentes/Exercicio1"
 import Exercicio2 from "./Componentes/Exercicio2"
 import Exercicio3 from "./Componentes/Exercicio3"
+import Exercicio4 from "./Componentes/Exercicio4"
+import Exercicio5 from "./Componentes/Exercicio5"
 
 export default function App()
 {
@@ -28,6 +30,15 @@ export default function App()
       <Exercicio3 valor={100} taxa={10} tempo={2} />
       <Exercicio3 valor={100} taxa={10} tempo={10} />
 
+      <hr />
+      <h1>Exercicio 4</h1>
+      <Exercicio4 tipo="retangulo" base={100} altura={5} />
+      <Exercicio4 tipo="triangulo" base={100} altura={5} />
+
+      <hr />
+      <h1>Exercicio 5</h1>
+      <Exercicio5 quantidade="10" />
+      <Exercicio5 quantidade="20" />
 
     </div>
   )
